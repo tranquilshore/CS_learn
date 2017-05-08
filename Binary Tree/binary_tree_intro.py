2e60234703fd5e1911c51880411a9eca2411fc6d
Sahil Thakur <sahilthakur@Baymax-2.local>
2017-05-09 01:04:34 +0530

    added binary tree

---
+++ Binary Tree/binary_tree_intro.py
@@ -0,0 +1,10 @@
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.val = key
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
